83eac7416639b954bac85929bd843ad79b6eee0c
Almiraadnan <almiraadnan34@gmail.com>
2024-07-22 09:51:52 -0700

    some-code

--- controllers/user.controller.mjs
+++ controllers/user.controller.mjs
@@ -41,6 +41,7 @@ export const deleteUser = async (req, res, next) => {
         return next(errorHandler(401, "You can only delete your account!"))
     }
     try {
+        console.log(req.params.id);
         await User.findByIdAndDelete(req.params.id)
         res.status(200).json('User has been deleted...')
     } catch (error) {
